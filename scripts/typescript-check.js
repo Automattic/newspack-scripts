2eea43f4ce32bb1ca3876396d70b92bf6fba6f37
Adam Borowski <adam@adamboro.com>
2022-02-03 15:06:30 +0100

    fix: typescript

--- scripts/typescript-check.js
+++ scripts/typescript-check.js
@@ -4,15 +4,9 @@ const spawn = require("cross-spawn");
 const path = require("path");
 const modules = require("./utils/modules");
 
-const result = spawn.sync(
-  `${process.cwd()}/node_modules/.bin/tsc`,
-  ["--project", path.resolve(__dirname, "../config/tsconfig.json")],
-  {
-    cwd: modules.rootDirectory,
-    stdio: "inherit",
-    env: process.env,
-  }
-);
+const result = spawn.sync(`${process.cwd()}/node_modules/.bin/tsc`, [], {
+  stdio: "inherit",
+});
 
 console.log(result);
 
