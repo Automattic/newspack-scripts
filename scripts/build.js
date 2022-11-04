2c149920f872bc1ed23a2cd91e524bc10fffb3be
Adam Borowski <adam@adamboro.com>
2022-11-04 13:46:29 +0100

    fix: it

--- scripts/build.js
+++ scripts/build.js
@@ -7,22 +7,19 @@ const utils = require("./utils/index.js");
 utils.log("Starting to build…");
 
 const calypsoReinstallResult = spawn.sync(
-  process.execPath,
-  [
-    "cd",
-    "node_modules/newspack-scripts",
-    "&&",
-    "npm",
-    "install",
-    "--legacy-peer-deps",
-  ],
+  "cd node_modules/newspack-scripts && npm i @automattic/calypso-build --legacy-peer-deps",
+  [],
   {
+    shell: true,
     cwd: modules.rootDirectory,
     stdio: "inherit",
   }
 );
 if (calypsoReinstallResult.status === 0) {
   utils.log("Calypso reinstall successful.");
+} else {
+  utils.log("Calypso reinstall failed:");
+  console.log(calypsoReinstallResult);
 }
 
 const buildResult = spawn.sync(process.execPath, [modules.calypsoBuild], {
