ca292458f6e165e5368cf4ad1913922fcffa2ef3
Adam Borowski <adam@adamboro.com>
2022-11-04 13:20:22 +0100

    fix(build): reinstall calypso-build before building

--- scripts/build.js
+++ scripts/build.js
@@ -4,14 +4,35 @@ const spawn = require("cross-spawn");
 const modules = require("./utils/modules");
 const utils = require("./utils/index.js");
 
-const result = spawn.sync(process.execPath, [modules.calypsoBuild], {
+utils.log("Starting to build…");
+
+const calypsoReinstallResult = spawn.sync(
+  process.execPath,
+  [
+    "cd",
+    "node_modules/newspack-scripts",
+    "&&",
+    "npm",
+    "install",
+    "--legacy-peer-deps",
+  ],
+  {
+    cwd: modules.rootDirectory,
+    stdio: "inherit",
+  }
+);
+if (calypsoReinstallResult.status === 0) {
+  utils.log("Calypso reinstall successful.");
+}
+
+const buildResult = spawn.sync(process.execPath, [modules.calypsoBuild], {
   cwd: modules.rootDirectory,
   stdio: "inherit",
   env: { ...process.env, NODE_ENV: "production" },
 });
 
-if (result.status === 0) {
+if (buildResult.status === 0) {
   utils.log("Build succeeded!");
 }
 
-process.exit(result.status);
+process.exit(buildResult.status);
