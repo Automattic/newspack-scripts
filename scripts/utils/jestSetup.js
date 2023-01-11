21eb37c7a1830f895089d00ee90fedf8c72742d6
Adam Borowski <adam@adamboro.com>
2023-01-11 12:36:12 +0100

    feat(test): mock IntersectionObserver

--- scripts/utils/jestSetup.js
+++ scripts/utils/jestSetup.js
@@ -3,7 +3,7 @@ import "@testing-library/jest-dom";
 // matchMedia does not exist in JSDOM, see https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
 Object.defineProperty(window, "matchMedia", {
   writable: true,
-  value: jest.fn().mockImplementation(query => ({
+  value: jest.fn().mockImplementation((query) => ({
     matches: false,
     media: query,
     onchange: null,
@@ -11,6 +11,32 @@ Object.defineProperty(window, "matchMedia", {
     removeListener: jest.fn(), // deprecated
     addEventListener: jest.fn(),
     removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn()
-  }))
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+// IntersectionObserver does not exist in JSDOM.
+// https://stackoverflow.com/a/58651649/3772847
+class MockIntersectionObserver {
+  constructor() {
+    this.root = null;
+    this.rootMargin = "";
+    this.thresholds = [];
+    this.disconnect = () => null;
+    this.observe = () => null;
+    this.takeRecords = () => [];
+    this.unobserve = () => null;
+  }
+}
+
+Object.defineProperty(window, "IntersectionObserver", {
+  writable: true,
+  configurable: true,
+  value: MockIntersectionObserver,
+});
+
+Object.defineProperty(global, "IntersectionObserver", {
+  writable: true,
+  configurable: true,
+  value: MockIntersectionObserver,
 });
