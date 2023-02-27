520204665b97b6f560aed9d17480678671ae6e09
Adam Borowski <aborowski24@gmail.com>
2023-02-27 13:21:08 +0100

    feat(stylelint): replace @wordpress/stylelint-config with stylelint-config-standard

--- config/stylelint.config.js
+++ config/stylelint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["@wordpress/stylelint-config/scss"],
+  extends: ["stylelint-config-standard"],
   rules: {
     "rule-empty-line-before": null,
     "at-rule-empty-line-before": null,
