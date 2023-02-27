bf413e261e558e5e98a1615c8b0b5af0800fed6d
Adam Borowski <aborowski24@gmail.com>
2023-02-27 13:28:58 +0100

    feat(stylelint): configuration

--- config/stylelint.config.js
+++ config/stylelint.config.js
@@ -1,17 +1,15 @@
 module.exports = {
   extends: ["stylelint-config-standard"],
+  ignoreAtRules: ["use", "include", "mixin"],
   rules: {
     "rule-empty-line-before": null,
     "at-rule-empty-line-before": null,
     "comment-empty-line-before": null,
-    "string-quotes": "single",
     "no-descending-specificity": null,
     "function-url-quotes": null,
     "font-weight-notation": null,
     "color-named": null,
-    "function-parentheses-space-inside": "always-single-line",
-    "media-feature-parentheses-space-inside": "always",
-    "selector-pseudo-class-parentheses-space-inside": "always",
     "selector-class-pattern": null,
+    "custom-property-pattern": null,
   },
 };
