2bb1dbcca75ddb24deb5f2b88cd2d7c7914e5453
Adam Boro <aborowski24@gmail.com>
2023-02-28 10:26:27 +0100

    feat(scss-linting): configuration for stylelint@15 (#182)
    BREAKING CHANGE: stylelint has to be updated to v15 on repos using this version

--- config/stylelint.config.js
+++ config/stylelint.config.js
@@ -1,20 +1,29 @@
 module.exports = {
-  extends: [
-    "@wordpress/stylelint-config/scss",
-    "stylelint-prettier/recommended",
-  ],
+  extends: ["stylelint-config-standard"],
   rules: {
     "rule-empty-line-before": null,
     "at-rule-empty-line-before": null,
     "comment-empty-line-before": null,
-    "string-quotes": "single",
     "no-descending-specificity": null,
     "function-url-quotes": null,
     "font-weight-notation": null,
     "color-named": null,
-    "function-parentheses-space-inside": "always-single-line",
-    "media-feature-parentheses-space-inside": "always",
-    "selector-pseudo-class-parentheses-space-inside": "always",
     "selector-class-pattern": null,
+    "custom-property-pattern": null,
+    "at-rule-no-unknown": null,
+    "alpha-value-notation": null,
+    "color-function-notation": null,
+    "function-no-unknown": [
+      true,
+      {
+        ignoreFunctions: ["/color/"],
+      },
+    ],
+    "annotation-no-unknown": [
+      true,
+      {
+        ignoreAnnotations: ["/default/"],
+      },
+    ],
   },
 };
