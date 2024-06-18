ca2a119aaa8fe7a389399144f83f3faf05bdad63
Adam Cassis <adam.cassis@automattic.com>
2024-06-18 10:30:46 +0200

    feat: use wp-scripts instead of calypso-build

--- config/babel.config.js
+++ config/babel.config.js
@@ -1,9 +1,6 @@
-module.exports = api => {
+module.exports = (api) => {
   api.cache(true);
   return {
-    presets: [
-      "@automattic/calypso-build/babel/default",
-      "@automattic/calypso-build/babel/wordpress-element"
-    ]
+    presets: ["@wordpress/babel-preset-default"],
   };
 };
--- scripts/utils/modules.js
+++ scripts/utils/modules.js
@@ -1,6 +1,17 @@
 const fs = require("fs");
 
+const rootDirectory = fs.realpathSync(process.cwd());
+
 module.exports = {
-  rootDirectory: fs.realpathSync(process.cwd()),
-  calypsoBuild: require.resolve("@automattic/calypso-build/bin/calypso-build"),
+  rootDirectory,
+  args: (cmd, opts = []) => {
+    const defaults = [cmd, "--config", "webpack.config.js"];
+
+    // Default build path: ./dist
+    if (!opts.includes("--output-path")) {
+      defaults.push("--output-path", "dist");
+    }
+
+    return [...defaults, ...opts];
+  },
 };
