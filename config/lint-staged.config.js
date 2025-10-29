e82cc321ba829039041b94aa588c664d93904f28
dkoo <derrick.koo@automattic.com>
2025-10-29 09:27:47 -0600

    chore: delete shared lint-staged config (not working)

--- config/lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	'lint-staged': {
-		'*.scss': 'npm run lint:scss:staged',
-		'*.(js|jsx)': 'npm run lint:js:staged',
-		'*.(ts|tsx)': 'npm run typescript:check',
-		'*.php': 'npm run lint:php:staged',
-	},
-};
