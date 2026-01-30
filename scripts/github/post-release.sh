65fb7e1f991a33c09a403c7e1080d049c0bde1ba
Miguel Peixe <miguel@peixe.co>
2026-01-30 13:57:24 -0300

    feat: support github actions (#225)
    Co-authored-by: Ramon Corrales <ramon.corrales@automattic.com>

---
+++ scripts/github/post-release.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+# This script should be ran on CI after a new regular (not pre-release) version is released.
+
+git config user.name "$GIT_COMMITTER_NAME"
+git config user.email "$GITHUB_COMMITER_EMAIL"
+
+# The last commit message at this point is the automated release commit. The second-to-last
+# commit message should contain data about the merge.
+SECOND_TO_LAST_COMMIT_MSG=$(git log -n 1 --skip 1 --pretty=format:"%s")
+
+LATEST_VERSION_TAG=$(git describe --tags --abbrev=0)
+
+git pull origin release
+git checkout alpha
+
+# If the merge was from alpha branch (the basic flow), alpha branch should be reset.
+if [[ $(echo $SECOND_TO_LAST_COMMIT_MSG | grep '^Merge .*alpha') ]]; then
+  echo '[newspack-scripts] Release was created from the alpha branch. Alpha branch will now be reset.'
+
+  # Reset the tip of alpha branch to the release branch.
+  # The alpha branch is single-serving, just for alpha releases. After a release,
+  # we don't care about any alpha changes.
+  git reset --hard release --
+  # Force-push the alpha branch.
+  git push "https://$GITHUB_TOKEN@github.com/${GITHUB_REPOSITORY}.git" --force
+else
+  echo '[newspack-scripts] Release was created from a different branch than the alpha branch (e.g. a hotfix branch).'
+  echo '[newspack-scripts] Alpha branch will now be updated with the lastest changes from release.'
+  git merge --no-ff release -m "chore(release): merge in release $LATEST_VERSION_TAG"
+  if [[ $? == 0 ]]; then
+    git push "https://$GITHUB_TOKEN@github.com/${GITHUB_REPOSITORY}.git"
+  else
+    git merge --abort
+    echo '[newspack-scripts] Post-release merge to alpha failed.'
+    if [ -z "$SLACK_CHANNEL_ID" ] || [ -z "$SLACK_AUTH_TOKEN" ]; then
+      echo '[newspack-scripts] Missing Slack channel ID and/or token. Cannot notify.'
+    else
+      echo '[newspack-scripts] Notifying the team on Slack.'
+      curl \
+        --data "{\"channel\":\"$SLACK_CHANNEL_ID\",\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"⚠️ Post-release merge to alpha failed for: \`$CIRCLE_PROJECT_REPONAME\`. Check <$CIRCLE_BUILD_URL|the build> for details.\"}}]}" \
+        -H "Content-type: application/json" \
+        -H "Authorization: Bearer $SLACK_AUTH_TOKEN" \
+        -X POST https://slack.com/api/chat.postMessage \
+        -s > /dev/null
+    fi
+  fi
+fi
+
+# Update trunk branch with latest changes from the release branch, so they are in sync.
+echo '[newspack-scripts] Merging the release branch into trunk.'
+git checkout trunk
+
+# Merge release branch into trunk branch, and notify the team if any conflicts arise.
+git merge --no-ff release -m "chore(release): merge in release $LATEST_VERSION_TAG"
+if [[ $? == 0 ]]; then
+  echo '[newspack-scripts] Pushing updated trunk to origin.'
+  git push "https://$GITHUB_TOKEN@github.com/${GITHUB_REPOSITORY}.git"
+else
+  git merge --abort
+  echo '[newspack-scripts] Post-release merge to trunk failed.'
+  if [ -z "$SLACK_CHANNEL_ID" ] || [ -z "$SLACK_AUTH_TOKEN" ]; then
+    echo '[newspack-scripts] Missing Slack channel ID and/or token. Cannot notify.'
+  else
+    echo '[newspack-scripts] Notifying the team on Slack.'
+    curl \
+      --data "{\"channel\":\"$SLACK_CHANNEL_ID\",\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"⚠️ Post-release merge to \`trunk\` failed for: \`$CIRCLE_PROJECT_REPONAME\`. Check <$CIRCLE_BUILD_URL|the build> for details.\"}}]}" \
+      -H "Content-type: application/json" \
+      -H "Authorization: Bearer $SLACK_AUTH_TOKEN" \
+      -X POST https://slack.com/api/chat.postMessage \
+      -s > /dev/null
+  fi
+fi
---
+++ scripts/github/release.js
@@ -0,0 +1,165 @@
+'use strict';
+
+const utils = require( './utils/index.js' );
+
+const semanticRelease = require( 'semantic-release' );
+
+const { files, ...otherArgs } = require( 'yargs/yargs' )(
+	process.argv.slice( 2 )
+).parse();
+
+const filesList = files.split( ',' );
+
+// Get repository name from GitHub Actions environment variable (format: owner/repo).
+const repoName = process.env.GITHUB_REPOSITORY?.split( '/' )[ 1 ] || 'unknown';
+
+utils.log( `Releasing ${ repoName }…` );
+
+const getConfig = ({ gitBranchName }) => {
+	const branchType = gitBranchName.split("/")[0];
+	const githubConfig = {
+		assets: [
+			{
+				path: `./release/${ repoName }.zip`,
+				label: `${ repoName }.zip`,
+			},
+		],
+		releasedLabels: false,
+	};
+
+	// Only post GH PR comments for alpha, hotfix/*, and release branches.
+	if ( ! ["alpha", "hotfix", "release"].includes(branchType) ) {
+		githubConfig.successComment = false;
+		githubConfig.failComment = false;
+	}
+
+	// Only publish alpha and release branches to NPM.
+	const shouldPublishOnNPM = Boolean( process.env.NPM_TOKEN ) && ["alpha", "release"].includes(branchType);
+	if ( shouldPublishOnNPM ) {
+		utils.log( `Will publish to npm.` );
+	}
+
+	const config = {
+		dryRun: otherArgs.dryRun,
+		ci: otherArgs.ci,
+		debug: otherArgs.debug,
+
+		branches: [
+			// `release` branch is published on the main distribution channel (a new version on GH).
+			"release",
+			// `alpha` branch – for regular pre-releases.
+			{
+				name: "alpha",
+				prerelease: true,
+			},
+			// `hotfix/*` branches – for releases outside of the release schedule.
+			{
+				name: "hotfix/*",
+				// With `prerelease: true`, the `name` would be used for the pre-release tag. A name with a `/`
+				// is not valid, though. See https://semver.org/#spec-item-9.
+				prerelease: '${name.replace(/\\//g, "-")}',
+			},
+			// `epic/*` branches – for beta testing/QA pre-release builds.
+			{
+				name: "epic/*",
+				// With `prerelease: true`, the `name` would be used for the pre-release tag. A name with a `/`
+				// is not valid, though. See https://semver.org/#spec-item-9.
+				prerelease: '${name.replace(/\\//g, "-")}',
+			},
+		],
+		prepare: ["@semantic-release/changelog", "@semantic-release/npm"],
+		plugins: [
+			"@semantic-release/commit-analyzer",
+			"@semantic-release/release-notes-generator",
+			[
+				// Whether to publish on npm.
+				"@semantic-release/npm",
+				{
+					npmPublish: shouldPublishOnNPM,
+				},
+			],
+			"semantic-release-version-bump",
+			// Add the built ZIP archive to GH release.
+			[
+				"@semantic-release/github",
+				githubConfig,
+			],
+		],
+	};
+
+	// Bump the semver and prepare a build package.
+	config.prepare.push( [
+		// Increment the version in additional files, and the create the release archive.
+		'semantic-release-version-bump',
+		{
+			files: filesList,
+			callback: 'npm run release:archive',
+		},
+	] );
+
+	// Unless on a hotfix or epic branch, add a commit that updates the files.
+	if ( [ 'hotfix', 'epic' ].indexOf( branchType ) === -1 ) {
+		let assets = filesList;
+		// These assets should be added to source control after a release.
+		if ( branchType === 'release' ) {
+			assets = [
+				...filesList,
+				'package.json',
+				'package-lock.json',
+				'CHANGELOG.md',
+			];
+		}
+		utils.log(
+			`On ${ branchType } branch, following files will be updated: ${ assets.join(
+				', '
+			) }`
+		);
+		config.prepare.push( {
+			path: '@semantic-release/git',
+			assets,
+			message:
+				'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+		} );
+	} else {
+		utils.log(
+			`This branch is ${ branchType }, plugin files and the changelog will *not* be updated.`
+		);
+	}
+
+	return config;
+};
+
+const run = async () => {
+	try {
+		const gitBranch = await utils.getGitBranch();
+
+		const result = await semanticRelease.default(
+			getConfig( { gitBranchName: gitBranch } )
+		);
+
+		if ( result ) {
+			const { lastRelease, commits, nextRelease, releases } = result;
+
+			utils.log(
+				`Published ${ nextRelease.type } release version ${ nextRelease.version } containing ${ commits.length } commits.`
+			);
+
+			if ( lastRelease.version ) {
+				utils.log( `The last release was "${ lastRelease.version }".` );
+			}
+
+			for ( const release of releases ) {
+				utils.log(
+					`The release was published with plugin "${ release.pluginName }".`
+				);
+			}
+		} else {
+			utils.log( 'No release published.' );
+		}
+	} catch ( err ) {
+		console.error( 'The automated release failed with %O', err );
+		process.exit( 1 );
+	}
+};
+
+run();
